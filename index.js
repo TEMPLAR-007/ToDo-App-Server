0625a29ab562aff22f2e6f0942b7766a0e9ae688
ashikmahmud <ashikmamud187@gmail.com>
2022-05-19 03:52:36 +0600

    added

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const app = require("./app");
+
 // port 
 const port = process.env.PORT || 5000;
-
 
 /* Test Listen Port */
 app.listen(port, ()=>{
     console.log(`TASK TODO SERVER RUNNING ON ${port}`);
-    
 });

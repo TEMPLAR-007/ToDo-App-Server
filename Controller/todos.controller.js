fe9ef8229baedea506c7787906ba91a8c224a2e3
ashikmahmud <ashikmamud187@gmail.com>
2022-05-19 02:29:02 +0600

    create new endpoints to delete todos

--- Controller/todos.controller.js
+++ Controller/todos.controller.js
@@ -1,4 +1,5 @@
 /* get all the treatments */
+const { ObjectId } = require("mongodb");
 const client = require("../Connection/connection");
 const todosCollection = client.db('todos-db').collection("todos");
 const postTodos = async(req, res) => {
@@ -16,7 +17,34 @@ const postTodos = async(req, res) => {
     }
 }
 
+const getTodos = async(req, res) => {
+    await client.connect();
+    const uid = req.query.uid;
+    const decodedId = req.decoded.uid;
+    if(uid === decodedId){
+        const query = {"author.uid": uid};
+        const result = await todosCollection.find(query).toArray();
+        if(result){
+            res.send({success:true, result})
+        }
+    }else{
+        res.status(403).send({success:false, message: "Forbidden Access."})
+    }
+}
 
 
+const deleteTodos = async(req,res) =>{
+    const postId = req.query.todoId;
+    const decodedId = req.decoded.uid;
+    const uid = req.query.uid;
+    if(decodedId === uid){
+        const result = await todosCollection.deleteOne({_id: ObjectId(postId)});
+        if(result.acknowledged){
+            res.send({success: true, message: "ToDo deleted successfully done."})
+        }
+    }else{
+        res.status(403).send({success:false, message: "Forbidden Access."})
+    }
+}
 
-module.exports = {postTodos}
+module.exports = {postTodos, getTodos, deleteTodos}
--- Routes/todos.route.js
+++ Routes/todos.route.js
@@ -1,8 +1,10 @@
 
 const router = require("express").Router();
-const { postTodos} = require("../Controller/todos.controller");
+const { postTodos, getTodos, deleteTodos} = require("../Controller/todos.controller");
 const VerifyToken = require("../VerifyToken/VerifyToken.js")
 /* Post Todos */
 router.post("/",VerifyToken, postTodos)
+router.get("/",VerifyToken, getTodos)
+router.delete("/",VerifyToken, deleteTodos)
 
 module.exports = router;

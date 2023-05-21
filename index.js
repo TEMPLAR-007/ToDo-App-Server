9ddfafc014078d9ab24e8e41ae11d8ebe3762fc5
unknown <arefin.khan8364@gmail.com>
2023-05-21 16:21:43 +0600

    port_change

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const app = require("./app");
 const port = process.env.PORT || 5000;
 
 /* Test Listen Port */
-app.listen(port, ()=>{
+app.listen(port, "0.0.0.0", () => {
     console.log(`TASK TODO SERVER RUNNING ON ${port}`);
 });
